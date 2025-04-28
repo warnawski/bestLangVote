2a9be8294d5f7175c34727a5da55fa5a0b44ad9a
Warnawski <barnawskidm@gmail.com>
2025-04-28 21:36:04 +0300

    Start integrate database

--- pkg/database/driver/mysql.go
+++ pkg/database/driver/mysql.go
@@ -1,9 +1,18 @@
 package driver
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+)
 
 type MysqlDriver struct{}
 
 func (msql *MysqlDriver) Connect(cfg DBConfig) (*sql.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tsp(%s:%s)/%s?parseTime=true",
+		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DBName)
+	return sql.Open("mysql", dsn)
+}
 
+func (mysql *MysqlDriver) GetPlaceholder(index int) string {
+	return "?"
 }
--- pkg/database/driver/posgresql.go
+++ pkg/database/driver/posgresql.go
@@ -1,9 +1,18 @@
 package driver
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+)
 
 type PostgresqlDriver struct{}
 
 func (psql *PostgresqlDriver) Connect(cfg DBConfig) (*sql.DB, error) {
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBName, cfg.SSLMode)
+	return sql.Open("postgres", dsn)
+}
 
+func (psql *PostgresqlDriver) GetPlaceholder(index int) string {
+	return fmt.Sprintf("$%s", index)
 }
